3964b18464d4425bf607ce85c26cfad5c86fcec2
渟雲 <zuiun1337@gmail.com>
2025-09-29 15:45:32 +0800

    LICENSE: Update to TOSSRCU 2025.9

--- Functions/Func.h
+++ Functions/Func.h
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) 2025 渟雲. All rights reserved.
 //
-// Licensed under the TOSSRCU License (the "License");
+// Licensed under the TOSSRCU 2025.9 License (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://github.com/M3351AN/M3351AN/blob/main/LICENSE
+//  https://raw.githubusercontent.com/M3351AN/M3351AN/1ee25fbd5318d178d15924046fa2060e765b2f66/LICENSE
 //
 // -----------------------------------------------------------------------------
 // File: Func.h
